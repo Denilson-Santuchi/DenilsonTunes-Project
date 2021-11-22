0d8fb13e9df016440ee6dbda08faf96b3e740da3
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2021-11-22 18:14:43 -0300

    third requirement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Links from './components/Links';
 import Routes from './components/Routes';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Links />
         <Routes />
       </BrowserRouter>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Load from './Load';
+
+export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      loading: true,
+    };
+
+    this.user = this.user.bind(this);
+  }
+
+  componentDidMount() {
+    this.user();
+  }
+
+  async user() {
+    const { name } = await getUser();
+    this.setState({ name, loading: false });
+  }
+
+  render() {
+    const {
+      state: {
+        name,
+        loading,
+      },
+    } = this;
+
+    return (
+      <div>
+        <header data-testid="header-component">
+          {loading ? <Load /> : (
+            <div data-testid="header-user-name">{name}</div>
+          )}
+        </header>
+      </div>
+    );
+  }
+}
