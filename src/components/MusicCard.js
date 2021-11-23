a03009b56596c561974d06c630642966209c9d24
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2021-11-23 13:16:14 -0300

    eighth requirement

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,32 +1,66 @@
-// requisito 7 feito em grupo com Igor Hamzi, Luis Fllipe e Raphael Oliveira.
+// requisito 7 e 8 feito em grupo com Igor Hamzi, Luis Fllipe e Raphael Oliveira.
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Load from './Load';
+import { addSong } from '../services/favoriteSongsAPI';
 
 export default class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      check: false,
+      loading: false,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  async onInputChange() {
+    const { music: { trackId } } = this.props;
+    this.setState({ loading: true, check: true });
+    await addSong(trackId);
+    this.setState({ loading: false });
+  }
+
   render() {
-    const { musicList } = this.props;
+    const {
+      state: {
+        check,
+        loading,
+      },
+      onInputChange,
+    } = this;
+    const { music: { trackName, previewUrl, trackId } } = this.props;
     return (
       <div>
-        {musicList.slice([1]).map(({ trackName, previewUrl, artistId }) => (
-          <>
-            <h4 key={ artistId }>{trackName}</h4>
-            <audio
-              data-testid="audio-component"
-              src={ previewUrl }
-              controls
-            >
-              <track kind="captions" />
-              O seu navegador não suporta o elemento
-              <code>audio</code>
-            </audio>
-          </>
-        ))}
+        <span>{trackName}</span>
+        <audio
+          data-testid="audio-component"
+          src={ previewUrl }
+          controls
+        >
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+        </audio>
+        {loading ? <Load /> : (
+          <label htmlFor="favoriteMusic">
+            Favorita
+            <input
+              data-testid={ `checkbox-music-${trackId}` }
+              id={ trackId }
+              type="checkbox"
+              checked={ check }
+              onChange={ onInputChange }
+            />
+          </label>
+        )}
       </div>
     );
   }
 }
 
 MusicCard.propTypes = {
-  musicList: PropTypes.string.isRequired,
+  music: PropTypes.string.isRequired,
 };
