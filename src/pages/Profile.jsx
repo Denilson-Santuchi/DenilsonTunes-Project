2bc3fc93f092fdf60d7bfb7b702bbc8696bbb7b7
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2021-12-26 17:48:39 -0300

    thirteenth requiment

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Load from '../components/Load';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      userEmail: 'email@test.com',
+      userImage: 'url-to-image',
+      userDescription: 'Lorem ipsum',
+      loading: false,
+    };
+
+    this.user = this.user.bind(this);
+  }
+
+  componentDidMount() {
+    this.user();
+  }
+
+  user() {
+    this.setState({ loading: true }, () => {
+      getUser()
+        .then(({ name }) => this.setState({
+          userName: name,
+          loading: false,
+        }));
+    });
+  }
+
   render() {
+    const {
+      loading,
+      userName,
+      userDescription,
+      userEmail,
+      userImage,
+    } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {loading ? <Load /> : (
+          <section>
+            <div>
+              <image
+                data-testid="profile-image"
+                src={ userImage }
+              />
+              <Link to="profile/edit">Editar perfil</Link>
+            </div>
+            <h2>{userName}</h2>
+            <h2>{userEmail}</h2>
+            <h2>{userDescription}</h2>
+          </section>
+        )}
       </div>
     );
   }
